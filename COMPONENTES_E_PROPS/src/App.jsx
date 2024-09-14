0d54d7fe1df6c9fc10b2b7b2a0b048edd440a715
mabiamabia <duartecostap@gmail.com>
2024-09-13 21:04:38 -0300

    feat: adiciona props multiplas

--- COMPONENTES_E_PROPS/src/App.jsx
+++ COMPONENTES_E_PROPS/src/App.jsx
@@ -1,10 +1,9 @@
 import HelloWorld from './components/HelloWorld'
 import HelloName from './components/Name'
+import Date from './components/Date'
 import './App.css'
 
 function App() {
-
-    
   return (
       <> 
       <h2>1 - Criando um componente simples</h2>
@@ -12,6 +11,9 @@ function App() {
 
       <h2>2 - Criando um componente com props</h2>
       <HelloName name={"Mabiá"}/>
+
+      <h2>3 - Criando um componente com três props</h2>
+      <Date day={"3"} month={"12"} year={"1992"}/>
       </>
   )
   
---
+++ COMPONENTES_E_PROPS/src/components/Date.jsx
@@ -0,0 +1,7 @@
+// eslint-disable-next-line react/prop-types
+const Date = ({ day, month, year }) => {
+    return <p>My date of birth is, {day} / {month} / {year}!</p>;
+  };
+  
+  export default Date;
+  
